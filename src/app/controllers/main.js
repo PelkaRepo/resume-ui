07b28d5ddf66ab6a7f3f1ae6fd166ef95813a26a
arnoldpelka <arnoldpelka@gmail.com>
2015-07-27 23:24:20 -0400

    Updated with relevant base content for resume grid

--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -1,3 +1,10 @@
 angular.module('app').controller('main', [ '$scope', function($scope) {
-	$scope.technologies = [ 'jQuery', 'AngularJS' ];
+	$scope.job = "Software Engineer";
+	$scope.loc = "Richmond, VA";
+	$scope.level = "Sr. Associate";
+	$scope.other = "Another Component";
+	$scope.timeFrame = "Software Engineer";
+
+
+	$scope.qualifiers = [ 'This is a qualifier', 'Super cool action happened' ];
 } ]);
